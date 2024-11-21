b2a549a20c17a964e0f51a57f15d813471f12f64
laytonprend <59471663+laytonprend@users.noreply.github.com>
2024-11-21 15:51:20 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import streamlit as st
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
-st.title("Example Streamlit App :balloon:")
+st.title("My Parents New Healthy Diner :balloon:")
 st.write(
     """Replace this example with your own code!
     **And if you're new to Streamlit,** check
