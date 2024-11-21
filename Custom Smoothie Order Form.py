c17fca010a145875d79fa58c25cebd7dacfce664
laytonprend <59471663+laytonprend@users.noreply.github.com>
2024-11-21 15:37:59 +0000

    Add files via upload
    streamlit app made in workshop

---
+++ Custom Smoothie Order Form.py
@@ -0,0 +1,51 @@
+# Import python packages
+import streamlit as st
+from snowflake.snowpark.context import get_active_session
+from snowflake.snowpark.functions import col
+
+# Write directly to the app
+st.title("Example Streamlit App :balloon:")
+st.write(
+    """Replace this example with your own code!
+    **And if you're new to Streamlit,** check
+    out our easy-to-follow guides at
+    [docs.streamlit.io](https://docs.streamlit.io).
+    """
+)
+
+
+
+#option = st.selectbox(
+ #   "fruit?",
+  #  ("Banana", "Strawberries", "Peaches"),
+#)
+
+#st.write("Fave fruit = ", option)
+name_on_order=st.text_input('Name of Smoothie')
+st.write('The name on your smoothie will be:', name_on_order)
+session = get_active_session()
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+#st.dataframe(data=my_dataframe, use_container_width=True)
+
+
+ingredients_list=st.multiselect('Choose up to 5 ingredients:',my_dataframe, max_selections=5)
+if ingredients_list: # checks if null
+    #st.write(ingredients_list)
+    #st.text(ingredients_list)
+    ingredients_string=''
+    for fruit_chosen in ingredients_list:
+        ingredients_string+=fruit_chosen+' '
+    st.write(ingredients_string)
+
+    time_to_insert=st.button('Submit Order')
+
+    #sql
+    my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order) values(' """+ingredients_string+"""','"""+name_on_order+ """')"""
+  #  +"""')""" # no semicolon?
+    #st.write(my_insert_stmt)
+    #st.stop() #temp
+    if time_to_insert:
+        session.sql(my_insert_stmt).collect()
+        st.success('Your Smoothie is ordered! '+name_on_order, icon="✅")
+
+
