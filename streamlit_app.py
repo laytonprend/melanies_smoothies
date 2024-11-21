a42931dc7f3a887146ac4e8e0b39d062f3f8f20c
laytonprend <59471663+laytonprend@users.noreply.github.com>
2024-11-21 16:43:05 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,8 +30,11 @@ st.write('The name on your smoothie will be:', name_on_order)
 cnx=st.connection('snowflake')
 session = cnx.session()#get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width=True)
-st.stop() #temp
+#st.dataframe(data=my_dataframe, use_container_width=True)
+#st.stop() #temp
+pd_df=my_dataframe.to_pandas()
+st.dataframe(pd_df)
+st.stop()
 
 ingredients_list=st.multiselect('Choose up to 5 ingredients:',my_dataframe, max_selections=5)
 if ingredients_list: # checks if null
